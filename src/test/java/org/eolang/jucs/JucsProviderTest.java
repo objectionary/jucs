52a86c9586f334a362c9422fe7ce0487a4ebab39
volodya-lombrozo <volodya.lombrozo@gmail.com>
2026-06-08 10:44:13 +0300

    test(#84): Add tests for exposing local and nested classpath paths

--- src/test/java/org/eolang/jucs/JucsProviderTest.java
+++ src/test/java/org/eolang/jucs/JucsProviderTest.java
@@ -37,4 +37,18 @@ final class JucsProviderTest {
     void findsWithLocalGlob(final String file) {
         Assertions.assertEquals(file, "hey!\n");
     }
+
+    @ParameterizedTest
+    @ClasspathSource(value = "com/yegor256/jucs/foo", glob = "*.txt")
+    void exposesLocalPath(final String content, final String path) {
+        Assertions.assertFalse(content.isEmpty());
+        Assertions.assertEquals("a.txt", path);
+    }
+
+    @ParameterizedTest
+    @ClasspathSource(value = "com/yegor256/jucs", glob = "**/*.txt")
+    void exposesNestedPath(final String content, final String path) {
+        Assertions.assertFalse(content.isEmpty());
+        Assertions.assertEquals("foo/a.txt", path);
+    }
 }
